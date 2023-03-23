7f268490047ff30c7e17b1b1cd71c6cc5a7af477
jakiasultana2022 <rokeyasbd@gmail.com>
2023-03-23 11:12:46 +0600

    remove console

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+
 async function run (){
+  
 try{
     
     const appointmentCollection = client.db('appointment-form').collection('appointments');
